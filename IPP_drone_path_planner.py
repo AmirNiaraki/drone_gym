9178b6d35267b13a759ea844fd544b703f9f57e4
Amir Niaraki <amir.niaraki@musco.com>
2022-11-09 12:35:58 -0600

    fixed the multiprocessing

--- IPP_drone_path_planner.py
+++ IPP_drone_path_planner.py
@@ -16,6 +16,8 @@ from stable_baselines3.common.vec_env import VecFrameStack
 from stable_baselines3.common.evaluation import evaluate_policy
 import cv2
 from threading import Thread
+import threading
+from multiprocessing  import Process
 from sys import exit
 import time
 
@@ -33,25 +35,35 @@ WORLD_Z_min=60
 WORLD_Z_max=100
 SEEDS=200
 square_size_range=(1,10)
-FRAME_W=70
-FRAME_H=50
+FRAME_W=700
+FRAME_H=500
 FOV_X=60/2 #degrees for halve of the field of view horizontaly
 FOV_Y=47/2 #degrees for halve of the field of view verticaly
 
-class Environment(Env):
+class droneEnv():
     
-    def __init__(self):
+    def __init__(self, name):
+        self.name=name
+        self.battery=75
+        self.main_thread = threading.current_thread()
+        if self.name=='cont':
+            self.observation_space = Box(low=0, high=255,
+                                    shape=(FRAME_H, FRAME_W+1), dtype=np.uint8)
+        if self.name=='disc':
 ### action list for 2d: [0 ,1       ,2    ,3         ,4   ,5        ,6   ,7]
 ### action list for 2d: [up,up-right,right,right-down,down,down-left,left,left-top ]
-        self.action_space=Discrete(8)
+            self.action_space=Discrete(8)
         self.location=[100,100,60]
         self.world=self.world_genertor()
 ### for getting the frame to the agent at all times
-        self.thread=Thread(target=self.update_frame, args=())
-        self.thread.daemon=True
-        self.thread.start()        
+        self.thread=Thread(target=self.update_frame, args=(),daemon=True)
+        # self.thread.daemon=True
+        self.thread.start()
+        time.sleep(1)
+        print('environment is initialized')        
 
 
+        
         
     def step(self, action):
 ### defining navigation #######################################################        
@@ -62,6 +74,7 @@ class Environment(Env):
     def reset(self):
         self.world=self.world_genertor()
         self.state=init_state
+        self.battery=75
         ###for COARSE Coding there is an auxiliary function to get location from state
         # self.location=self.loc_from_state()
         self.location=[100,100,60]
@@ -103,13 +116,15 @@ class Environment(Env):
          return self.world
     
     def update_frame (self):
+        self.imager_thread_name=threading.current_thread()
+        print(self.imager_thread_name)
         while True:
             visible_x=tan(radians(FOV_X))*2*self.location[2]
             visible_y=tan(radians(FOV_Y))*2*self.location[2]
             world_img=np.uint8((1-self.world)*255)
-            # print(int(-visible_y/2+self.location[1]),int(visible_y/2+self.location[1]),int(-visible_x/2+self.location[0]),int(visible_x/2+self.location[0]) )
+            ### take snap of the sim based on location [x,y,z]
             crop=world_img[int(-visible_y/2+self.location[1]):int(visible_y/2+self.location[1]), int(-visible_x/2+self.location[0]):int(visible_x/2+self.location[0])]
-            resized=cv2.resize(crop, (700,500))
+            resized=cv2.resize(crop, (FRAME_W, FRAME_H))
             self.frame=resized
 
     def fetch_frame(self):
@@ -125,20 +140,56 @@ class Environment(Env):
     
     def close(self):
         self.thread.join()
+### method receives frame as np array adds a column the end that represent battery level    
+    def concat_battery(self):
+        input_frame=self.fetch_frame()
+        full_pixels=np.zeros([int(FRAME_H*self.battery/100), 1])
+        full_pixels.astype(int)
+        empty_pixels=(np.zeros([FRAME_H-int(FRAME_H*self.battery/100),1])+1)*255
+        empty_pixels.astype(int)
+
+        # print('len of full pixs: ',str(len(full_pixles())), 'empty pixs: ', str(len(empty_pixels)))
+        battery_img=np.uint8(np.concatenate((empty_pixels, full_pixels)))
+        cv2.imwrite('justB.png', battery_img)
+        self.output_frame=np.concatenate((input_frame, battery_img),axis=1)
+        # output_frame=np.append(input_frame,np.zeros([len(input_frame),1]),1)
+        print('in shape: ',input_frame.shape, 'out_shape: ', self.output_frame.shape)
+        return self.output_frame
+        
+        
+        
+        
          
             
 
-env=Environment()
+env=droneEnv('cont')
+
 W=env.world_genertor()
-# env.reset()
+# cv2.imwrite('withB.png', W)
+# 
+# frame1=env.fetch_frame()
+frame2=env.concat_battery()
+
+# cv2.imwrite('without battery', frame1)
+cv2.imwrite('withB.png', frame2)
+# env.close()
+exit()
+## closing all the threads except main
+# main_thread = threading.current_thread()
+# for t in threading.enumerate():
+#     if t is env.main_thread:
+#         continue
+#     print(t)
+#     t.join()
 
 
+### imshow as movie
 counter=1
 for i in range(0,500):
     try:
         env.location=[100+i,100,60]
         frame1=env.fetch_frame()
-        cv2.imshow('drone view', W)
+        cv2.imshow('drone view', frame1)
         time.sleep(0.1)
         
     except:
--- checkenv.py
+++ checkenv.py
@@ -5,3 +5,12 @@ Created on Mon Nov  7 20:08:03 2022
 @author: aniaraki
 """
 
+from stable_baselines3.common.env_checker import check_env
+from snakeenv import SnekEnv
+from IPP_drone_path_planner import dronEnv
+
+
+
+env = SnekEnv()
+# It will check your custom environment and output additional warnings if needed
+check_env(env)
